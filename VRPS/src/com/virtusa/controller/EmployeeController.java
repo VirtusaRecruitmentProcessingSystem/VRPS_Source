3ac08219d72696693fa550c9cf0667550c3205ef
vrps.teamk19 <vrps.teamk@gmail.com>
2019-09-25 09:56:52 +0530

    Adding Controller classes

---
+++ VRPS/src/com/virtusa/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.virtusa.controller;
+
+public class EmployeeController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ VRPS/src/com/virtusa/controller/JobseekerController.java
@@ -0,0 +1,10 @@
+package com.virtusa.controller;
+
+public class JobseekerController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
