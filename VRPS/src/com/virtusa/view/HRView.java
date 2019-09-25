3ea6e64a48d833029918f7efdfa45b5a43391b03
vrps.teamk19 <vrps.teamk@gmail.com>
2019-09-25 09:54:06 +0530

    Adding Views

---
+++ VRPS/src/com/virtusa/view/HRView.java
@@ -0,0 +1,10 @@
+package com.virtusa.view;
+
+public class HRView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ VRPS/src/com/virtusa/view/TRView.java
@@ -0,0 +1,10 @@
+package com.virtusa.view;
+
+public class TRView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
