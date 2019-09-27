099869565ffee55d1a56b210c79e2d4d7476bd58
admin <admin@1A-1>
2019-09-27 16:05:25 +0530

    asdad

--- VRPS/src/com/virtusa/controller/JobseekerController.java
+++ VRPS/src/com/virtusa/controller/JobseekerController.java
@@ -1,11 +1,15 @@
 package com.virtusa.controller;
 
 import com.virtusa.model.JobseekerModel;
+import com.virtusa.services.JobseekerServicesImpl;
 
 public class JobseekerController {
 
 public void registerJobSeeker(JobseekerModel model) {
 	
+	JobseekerServicesImpl register=new JobseekerServicesImpl();
+ 	register.JobseekerRegistration(model);
+ 	
 }
 
 }
--- VRPS/src/com/virtusa/services/JobseekerServices.java
+++ VRPS/src/com/virtusa/services/JobseekerServices.java
@@ -1,10 +1,7 @@
 package com.virtusa.services;
 
-public class JobseekerServices {
+import com.virtusa.model.JobseekerModel;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public interface JobseekerServices{
+	public void JobseekerRegistration(JobseekerModel model);
 }
