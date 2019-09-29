8a60f4411891f43dfb85f7b09bab04dcce128ebe
Lenovo <Lenovo@192.168.0.11>
2019-09-29 20:09:09 +0530

    vrps

--- VRPS/src/com/virtusa/dao/TrDAOImp.java
+++ VRPS/src/com/virtusa/dao/TrDAOImp.java
@@ -1,4 +1,4 @@
-package com.virtusa.dao;
+  package com.virtusa.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
--- VRPS/src/com/virtusa/view/JobseekerView.java
+++ VRPS/src/com/virtusa/view/JobseekerView.java
@@ -189,3 +189,4 @@ public class JobseekerView {
 		
 	}
 }
+ 
